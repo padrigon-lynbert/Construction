9ba5598d31444c4f83deab21fb2900730c802d33
siv <padrigonlynbert@gmail.com>
2026-03-04 19:52:27 +0800

    making comments

--- core/static/content_ajax.js
+++ core/static/content_ajax.js
@@ -1,4 +1,5 @@
 function loadContent(url) {
+    // click sidebar to get url, then fetch content and update the page
     fetch(url)
         .then(response => response.text())
         .then(data => {
--- core/static/sidebar.js
+++ core/static/sidebar.js
@@ -1,4 +1,5 @@
 function toggleSidebar() {
+    // Toggle the "collapsed" class on the sidebar element
     const sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("collapsed");
 }

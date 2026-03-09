b5e7c6fb05be62e2d65ff6ef11214e8a8fe55077
siv <padrigonlynbert@gmail.com>
2026-03-09 18:26:36 +0800

    connect models into individual project's page

--- quotation/models.py
+++ quotation/models.py
@@ -1,3 +1,22 @@
+from ast import Pass
+from os import name
+
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    client_name = models.CharField(max_length=255)
+    margin = models.FloatField(default=0)  # in percent
+    tax = models.FloatField(default=0)     # in percent
+
+
+class ProjectItem(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="items")
+    item_name = models.CharField(max_length=255)
+    quantity = models.FloatField(default=0)
+    unit_cost = models.FloatField(default=0)
+    
+    @property
+    def autocost(self):
+        return self.quantity * self.unit_cost
--- quotation/views.py
+++ quotation/views.py
@@ -1,6 +1,8 @@
+from multiprocessing import context
 from urllib import request
-
 from django.shortcuts import render
+
+from .models import Project
 
 # Create your views here.
 
@@ -8,6 +10,13 @@ def quotation(request):
     return render(request, "quotation.html")
 
 def individual_quotation(request):
-    return render(request, "individual_quotation.html")
+    project = Project.objects.first()
+    item = project.items if project else None
+
+    context = {
+        'project': project,
+        'item': item
+    }
+    return render(request, "individual_quotation.html", context)
 
 
